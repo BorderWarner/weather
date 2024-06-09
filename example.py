0c59ad86c83b027c5a429fa51038f44307c9235e
BorderWarner <147915786+BorderWarner@users.noreply.github.com>
2024-06-09 16:33:57 +0300

    min fix attr

--- example.py
+++ example.py
@@ -1,16 +1,20 @@
 from get_weather import get_weather
 import pandas as pd
 
-hourly_dataframe, latitude, longitude, elevation = get_weather(16, 56.062435, 159.903470)
+hourly_dataframe, latitude, longitude, elevation = get_weather(16, 55.710803, 37.473970)
 print(f"Координаты: {latitude} с.ш. {longitude} в.д.")
 print(f"Высота: {elevation} м")
 # print(hourly_dataframe)
 
-target_datetime = pd.to_datetime("2024-06-08 22:00:00", utc=True)
+target_datetime = pd.to_datetime("2024-06-09 16:00:00", utc=True)
 
 matching_row = hourly_dataframe[hourly_dataframe['date'] == target_datetime]
 
 if not matching_row.empty:
-    print(matching_row['temperature_2m'].values[0])
+    print('Температура: ', matching_row['temperature_2m'].values[0])
+    print('Влажность: ', matching_row['relative_humidity_2m'].values[0])
+    print('Скорость ветра: ', matching_row['wind_speed_10m'].values[0])
+    print('Облачность: ', matching_row['cloud_cover'].values[0])
+    print('Поверхностное давление: ', matching_row['surface_pressure'].values[0])
 else:
     print("Нет данных")
--- get_weather.py
+++ get_weather.py
@@ -16,7 +16,8 @@ def get_weather(forecast_days, latitude, longitude):
         "latitude": latitude,
         "longitude": longitude,
         "hourly": ["temperature_2m", "relative_humidity_2m", "apparent_temperature", "precipitation_probability",
-                   "precipitation", "rain", "snowfall", "snow_depth", "pressure_msl", "cloud_cover", "wind_speed_10m"],
+                   "precipitation", "rain", "snowfall", "snow_depth", "pressure_msl", "cloud_cover", "wind_speed_10m",
+                   "surface_pressure"],
         "timezone": "Europe/Moscow"
     }
     responses = openmeteo.weather_api(url, params=params)
@@ -35,6 +36,7 @@ def get_weather(forecast_days, latitude, longitude):
     hourly_pressure_msl = hourly.Variables(8).ValuesAsNumpy()
     hourly_cloud_cover = hourly.Variables(9).ValuesAsNumpy()
     hourly_wind_speed_10m = hourly.Variables(10).ValuesAsNumpy()
+    hourly_surface_pressure = hourly.Variables(11).ValuesAsNumpy()
 
     hourly_data = {"date": pd.date_range(
         start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
@@ -46,7 +48,7 @@ def get_weather(forecast_days, latitude, longitude):
         "precipitation_probability": hourly_precipitation_probability,
         "precipitation": hourly_precipitation, "rain": hourly_rain, "snowfall": hourly_snowfall,
         "snow_depth": hourly_snow_depth, "pressure_msl": hourly_pressure_msl, "cloud_cover": hourly_cloud_cover,
-        "wind_speed_10m": hourly_wind_speed_10m}
+        "wind_speed_10m": hourly_wind_speed_10m, "surface_pressure": hourly_surface_pressure}
 
     return pd.DataFrame(data=hourly_data), response.Latitude(), response.Longitude(), response.Elevation()
 
